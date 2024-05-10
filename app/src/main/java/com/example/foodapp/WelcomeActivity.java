ce10f5c426241f8e3320036169fa6f3fd880f6f9
HiHocCode <ngoctyho1612@gmail.com>
2024-05-11 00:21:19 +0700

    WelcomeScreen

--- app/src/main/java/com/example/foodapp/WelcomeActivity.java
+++ app/src/main/java/com/example/foodapp/WelcomeActivity.java
@@ -2,13 +2,24 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    private static final int SPLASH_DISPLAY_DURATION = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_DURATION);
     }
 }
