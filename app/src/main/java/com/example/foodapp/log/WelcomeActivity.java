692537ad40964849868618b8cacbba1c39d32cea
HiHocCode <ngoctyho1612@gmail.com>
2024-05-16 17:22:59 +0700

    Create Folder

--- app/src/main/java/com/example/foodapp/log/WelcomeActivity.java
+++ app/src/main/java/com/example/foodapp/log/WelcomeActivity.java
@@ -1,10 +1,13 @@
-package com.example.foodapp;
+package com.example.foodapp.log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import com.example.foodapp.MainActivity;
+import com.example.foodapp.R;
 
 public class WelcomeActivity extends AppCompatActivity {
     private static final int SPLASH_DISPLAY_DURATION = 2000;
@@ -16,7 +19,7 @@ public class WelcomeActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                Intent mainIntent = new Intent(WelcomeActivity.this, LoginActivity.class);
                 startActivity(mainIntent);
                 finish();
             }
